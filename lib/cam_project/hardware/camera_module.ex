f4026e53714e3ed38c4ea0ec2afbf5c788c0d1ee
Johan Gomez Benavides <johangb812@gmail.com>
2026-04-03 19:15:27 +0200

    Add video capture to Camera Module 3

--- lib/cam_project/hardware/camera_module.ex
+++ lib/cam_project/hardware/camera_module.ex
@@ -24,6 +24,26 @@ defmodule CamProject.Hardware.CameraModule do
     end
   end
 
+    def capture_video(duration_seconds \\ 5, output_path \\ nil) do
+    File.mkdir_p!(@snapshot_dir)
+
+    path = output_path || Path.join(@snapshot_dir, "cam_module_#{System.os_time(:second)}.h264")
+
+    case System.cmd("/usr/bin/libcamera-vid", [
+      "-o", path,
+      "-t", "#{duration_seconds * 1000}",
+      "--width", "1920",
+      "--height", "1080"
+    ], stderr_to_stdout: true) do
+      {_output, 0} ->
+        Logger.info("CameraModule: video saved to #{path}")
+        {:ok, path}
+      {output, code} ->
+        Logger.error("CameraModule: video failed (#{code}): #{output}")
+        {:error, output}
+    end
+  end
+
   def available? do
     File.exists?(@libcamera_still)
   end
