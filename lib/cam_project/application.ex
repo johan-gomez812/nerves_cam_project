480163017f55ae3febd8c132bac8982b9ccc4ffb
Johan Gomez Benavides <johangb812@gmail.com>
2026-04-03 22:23:55 +0200

    Make CameraStream manually controlled

--- lib/cam_project/application.ex
+++ lib/cam_project/application.ex
@@ -7,8 +7,7 @@ defmodule CamProject.Application do
       {CamProject.DetectionStore, []},
       {CamProject.Recorder, []},
       {CamProject.WebServer, []},
-      {CamProject.Pipeline, []},
-      {CamProject.Hardware.CameraStream, []}
+      {CamProject.Pipeline, []}
     ]
 
     opts = [strategy: :one_for_one, name: CamProject.Supervisor]
